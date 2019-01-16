237d95a59635ae666b922370bd596b09c5ede7d5
Emilie Douvier <emilie@macbook-air-de-emilie.home>
2019-01-16 20:02:42 +0100

    Installe et setup api-pagination

--- level2/app/controllers/api/communications_controller.rb
+++ level2/app/controllers/api/communications_controller.rb
@@ -1,4 +1,5 @@
 class Api::CommunicationsController < ApplicationController
+  include Rails::Pagination
 
   def create
     practitioner = Practitioner.where(first_name: communication_params[:first_name], last_name: communication_params[:last_name]).first
@@ -11,7 +12,13 @@ class Api::CommunicationsController < ApplicationController
   end
 
   def index
-    render json: Communication.all.to_json, status: :ok
+    # render json: Communication.paginate(:page => params[:page], per_page: 10)
+
+    # render json: Communication.all.to_json, status: :ok
+
+    communication = Communication.all
+
+    paginate json: communication, page: params[:page], per_page: 25
   end
 
   def communication_params
